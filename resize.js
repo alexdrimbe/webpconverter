fb3a8041e69b5b57ed89394c85f537e7c94c0a47
Alex Drimbe <alex@paymoapp.com>
2022-03-14 15:17:04 +0200

    feat: resize script

---
+++ resize.js
@@ -0,0 +1,46 @@
+const sharp = require('sharp')
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+const colors = require('colors')
+
+const inputDir = path.normalize(`${__dirname}/input`)
+const outputDir = path.normalize(`${__dirname}/resized`)
+
+colors.setTheme({
+	info: 'green',
+	error: 'red',
+})
+
+const files = path.join(inputDir, '/**/*.+(png|jpg|jpeg)')
+
+const resizeFile = (inputFile) => {
+	const fileParse = path.parse(inputFile)
+	const inputFileDir = path.normalize(fileParse?.dir)
+	const outputFileDir = inputFileDir?.replace(inputDir, outputDir)
+	const fileName = `${fileParse?.name}.webp`
+	const outputFile = path.join(outputFileDir, fileName)
+
+	if (!fs.existsSync(outputFileDir)) {
+		fs.mkdirSync(outputFileDir, { recursive: true })
+	}
+
+	sharp(inputFile)
+		.metadata()
+		.then(({ width }) =>
+			sharp(inputFile)
+				.resize(Math.round(width * 0.6))
+				.webp({ quality: 85 })
+				.toFile(outputFile)
+				.then((info) => {
+					console.log(colors.info(outputFile, info?.size))
+				})
+				.catch((error) => {
+					console.log(colors.error(inputFile, error))
+				})
+		)
+}
+
+glob(files, {}, (er, files) => {
+	files.forEach(resizeFile)
+})
